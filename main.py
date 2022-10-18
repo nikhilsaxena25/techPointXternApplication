90e728291b1e1ce1da545a07a2d14e95bcf54518
nikhilsaxena25 <saxena74@purdue.edu>
2022-10-18 00:40:43 -0400

    Converting to a dataFrame

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from serpapi import GoogleSearch
+import pandas as pd
 
 
-def top_2(arr, type):
+def top_2(arr, n=2):
     temp = []
     count = 0
     for items in arr:
-        if items[type] >= 4.0 and count < 2:
-            temp.append(items)
-            print(items)
-            count += 1
+        if 'rating' in items:
+            if items['rating'] >= 4.0 and count < n:
+                temp.append(items)
+                count += 1
     return temp
 
 
@@ -17,26 +18,92 @@ time_commit = []
 
 params = {
     "engine": "google_maps",
-    "q": "fun things to do",
+    "q": "",
     "ll": "@39.7683331,-86.1583502,15.1z",
     "type": "search",
     "api_key": "a37967b7a90a0398dd74eb7c1a42f8c6a6aadccdeb7ff3a23132c024ef106c3a"
 }
 
+params['q'] = "fun things to do"
 search = GoogleSearch(params)
 results = search.get_dict()
 local_results = results["local_results"]
-final_list.append(top_2(local_results, "rating"))
+final_list.append(top_2(local_results, 2))
 
 params['q'] = "thai food"
 search = GoogleSearch(params)
 results = search.get_dict()
 local_results = results["local_results"]
-final_list.append(top_2(local_results, "rating")[0])
-#
+final_list.append(top_2(local_results, 1))
+
 params['q'] = "shopping places"
 search = GoogleSearch(params)
 results = search.get_dict()
 local_results = results["local_results"]
-#print(local_results[0])
-final_list.append(top_2(local_results, "rating")[0])
+final_list.append(top_2(local_results, 2))
+
+params['q'] = "chinese food"
+search = GoogleSearch(params)
+results = search.get_dict()
+local_results = results["local_results"]
+final_list.append(top_2(local_results, 1))
+
+# print(final_list[0][0])
+times = []
+name = []
+address = []
+place_type = []
+rating = []
+timings = []
+website = []
+phone = []
+
+df1 = pd.DataFrame(columns=['Title', 'Type', 'Phone', 'Website', 'Rating', 'Address', 'Operation Hours'])
+
+
+def data_add(item):
+    ret_list = []
+    if 'title' in item:
+        ret_list.append(item['title'])
+    else:
+        ret_list.append(None)
+
+    if 'type' in item:
+        ret_list.append(item['type'])
+    else:
+        ret_list.append(None)
+
+    if 'phone' in item:
+        ret_list.append(item['phone'])
+    else:
+        ret_list.append(None)
+
+    if 'website' in item:
+        ret_list.append(item['website'])
+    else:
+        ret_list.append(None)
+
+    if 'rating' in item:
+        ret_list.append(item['rating'])
+    else:
+        ret_list.append(None)
+
+    if 'address' in item:
+        ret_list.append(item['address'])
+    else:
+        ret_list.append(None)
+
+    if 'operating_hours' in item:
+        ret_list.append(item['operating_hours'])
+    else:
+        ret_list.append(None)
+
+    return ret_list
+
+
+for items in final_list:
+    for i in items:
+        print(data_add(i))
+        df1.loc[len(df1)] = data_add(i)
+
+print(df1)
